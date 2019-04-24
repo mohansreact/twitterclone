4c103a4871dca37671d2d3fb72a4758426e6e1c4
mohankumar <mohankumar73@hotmail.com>
2019-04-24 13:08:14 +0000

    add messages

--- server/index.js
+++ server/index.js
@@ -5,13 +5,17 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
 const db = require("./models");
+
+
 const PORT = 8081;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users/:id/messages", messageRoutes);
 
 app.use(function(req, res, next) {
   let err = new Error("Not Found");
