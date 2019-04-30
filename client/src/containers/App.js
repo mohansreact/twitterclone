480a19eba7ed273e6fa2d72d174738b64d703a43
mohankumar <mohankumar73@hotmail.com>
2019-04-30 09:39:42 +0000

    create store

---
+++ client/src/containers/App.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { configureStore } from "../store";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const store = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div>Hello Twitter World!</div>
+    </Router>
+  </Provider>
+);
+
+export default App;
