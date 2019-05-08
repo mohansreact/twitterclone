3b416dd356118002529e54955bc0bf45ed9b0da8
mohankumar <mohankumar73@hotmail.com>
2019-05-08 07:52:52 +0000

    final commit

--- client/src/containers/MessageList.js
+++ client/src/containers/MessageList.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {fetchMessages} from "../store/actions/messages";
+import {fetchMessages, removeMessage} from "../store/actions/messages";
 import MessageItem from "../components/MessageItem";
 
 
@@ -9,9 +9,17 @@ class MessageList extends Component {
         this.props.fetchMessages();
     }
     render() {
-        const { messages } = this.props;
+        const { messages, removeMessage, currentUser } = this.props;
         let messageList = messages.map(m =>  (
-            <MessageItem key={m._id} date={m.createAt} text={m.text} username={m.user.username} profileImageUrl={m.user.profileImageUrl} />
+            <MessageItem 
+                key={m._id} 
+                date={m.createAt} 
+                text={m.text} 
+                username={m.user.username} 
+                profileImageUrl={m.user.profileImageUrl} 
+                removeMessage={removeMessage.bind(this, m.user._id, m._id)}
+                isCorrectUser={currentUser === m.user._id}
+                />
         ));
         return (
             <div className="row col-sm-8">
@@ -27,8 +35,9 @@ class MessageList extends Component {
 
 function mapStateToProps(state) {
     return {
-        messages: state.messages
+        messages: state.messages,
+        currentUser: state.currentUser.user.id
     };
 }
 
-export default connect(mapStateToProps, { fetchMessages })(MessageList);
+export default connect(mapStateToProps, { fetchMessages, removeMessage })(MessageList);
